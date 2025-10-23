cec423a5d558061cf30ef9f47a1dfec761351aed
Niwa <Niwa.Ifiok@gmail.com>
2025-10-23 16:23:50 +0200

    chore: add pytest and flake8 configuration files

---
+++ setup.py
@@ -0,0 +1,19 @@
+﻿from setuptools import setup, find_packages
+
+setup(
+    name="calculait",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "pandas",
+        "scikit-learn",
+    ],
+    extras_require={
+        "dev": [
+            "pytest>=7.4.0",
+            "pytest-cov>=4.1.0",
+            "flake8>=6.1.0",
+        ]
+    },
+)
